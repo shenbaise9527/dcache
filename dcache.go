5a19a3d8e66f2dbf861aef1a7c742339e690865e
shenbaise9527 <zhouyingan@gmail.com>
2020-04-29 17:31:17 +0800

    cors

--- dcache.go
+++ dcache.go
@@ -49,6 +49,7 @@ func main() {
 	// 中间件.
 	r.Use(GinLoggerMiddleware())
 	r.Use(GinRecoveryMiddleware())
+	r.Use(GinCorsMiddleware())
 
 	// 提供的http接口.
 	r.GET("get", func(ctx *gin.Context) {
@@ -154,4 +155,21 @@ func GinLoggerMiddleware() gin.HandlerFunc {
 // GinRecoveryMiddleware gin崩溃插件.
 func GinRecoveryMiddleware() gin.HandlerFunc {
 	return gin.RecoveryWithWriter(logger.GetLoggerWriter())
+}
+
+// GinCorsMiddleware 跨域插件.
+func GinCorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
 }
